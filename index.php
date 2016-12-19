56a9a77dfd7cfa67d1aa3f2d108723bc2bf74c28
Etudiant <Etudiant@L35-14>
2016-12-19 20:47:30 +0100

    Modif controleur (index)

--- index.php
+++ index.php
@@ -1,8 +1,24 @@
-<?php
+<!DOCTYPE html>
 
-echo 'Salut mec';
-
-echo 'Ca roule ?';
-
-?>
-<h1> salut toi </h1>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php 'include ./modeles/modeleCours.php'; ?>
+        <h1> Projet GIT ISFCE DVPWEB</h1>
+        <p> lundi 19-12-2016 </p>
+        
+        <h3>Liste des cours de l'ISFCE</h3>
+        
+        <?php
+        switch ($_GET["page"]) {
+            case 'liste_cours':
+                $cours = get_liste_cours();
+                include './vues/listeCours.php';
+                break;
+        }
+        ?>
+    </body>
+</html>
